9c263cdd9604a1fc5efe25d6d88c286cf28e687f
User <nadya11090@gmail.com>
2021-10-13 17:46:50 +0300

    I made changes to my homework

--- src/main/java/tasks/homework/TrainsMethodReturnRunner.java
+++ src/main/java/tasks/homework/TrainsMethodReturnRunner.java
@@ -7,7 +7,11 @@ public class RunnerReturn {
         TrainMethodsReturn f3 = new TrainMethodsReturn();
         f3.returnNewInt(2);
         f3.returnNewLong(45454545);
+        f3.returnNewChar();
         f3.returnNewFloat(0.3f);
+        f3.returnNewDouble(0.5f);
+        f3.returnNewShort(22);
+        f3.returnNewByte(2);
 
         //Bee f4 = new Bee("male",0.05 );
        // new TrainMethodsObjects().processBee(bee);
