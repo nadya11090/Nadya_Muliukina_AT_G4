62a5760414bb7db6c00e4a33b8616075c6846ab6
user <nadya11090@gmail.com>
2021-10-15 01:01:55 +0300

    I have finished subtask Base task

---
+++ src/main/java/tasks/homework/Obstacle.java
@@ -0,0 +1,30 @@
+package tasks.homework;
+
+public class Obstacle {
+    private String description;
+    private String severity;
+    public void printMouseDetails() {
+        System.out.println("Возникло " + this.severity + "препятствие " + this.description);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public Obstacle(String description, String severity) {
+        this.description = description;
+        this.severity = severity;
+    }
+}
