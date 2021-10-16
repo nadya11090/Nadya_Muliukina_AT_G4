41e4c4f040d97d2d1d04cd3022968b11d60eb84c
user <nadya11090@gmail.com>
2021-10-16 03:17:04 +0300

    I have finished subtask bubbles task

--- src/main/java/project/boxing/Bottle.java
+++ src/main/java/project/boxing/Bottle.java
@@ -4,5 +4,31 @@ public class Bottle {
     private double volume;
     private String SparklingWater;
 
+    public void open() {
+        this.water.setOpened(true);
+
+        System.out.printf("Change of state to: open").println();
+
+    }
+
+    public void warm(int temperature){
+
+        System.out.printf("Warming water to: %s", temperature).println();
+
+    }
+    public SparklingWater getWater() {
+        System.out.printf("Returning an object: water").println();
+        return getWater();
+    }
+
+    public setWater(SparklingWater water) {
+        System.out.printf("Adding a new object: water").println();
+        return Water();
+
+    }
+
+        public Bottle(double volume) {
+        this.volume = volume;
+    }
 
 }
---
+++ src/main/java/project/boxing/SparklingWater.java
@@ -0,0 +1,33 @@
+package project.boxing;
+
+public class SparklingWater extends Water {
+    private boolean isOpened;
+    private Bubble[] bubbles;
+
+    public SparklingWater(boolean isOpened) {
+        this.isOpened = isOpened;
+    }
+
+    public void pump(Bubble[] bubbles) {
+        System.out.printf("Saturation of water with gas").println();
+    }
+
+    public void setOpened(boolean isOpened) {
+        System.out.printf("Change of state of water to open").println();
+    }
+
+    private void isOpened() {
+
+        System.out.printf("Checking water for openness").println();
+    }
+
+    private degas() {
+        System.out.printf("Saturation of water with gas").println();
+        return;
+    }
+
+    public boolean isSparkle() {
+        System.out.printf("Test for gas content in water").println();
+    }
+
+}
