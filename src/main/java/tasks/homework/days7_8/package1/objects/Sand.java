c143d14fc8681cac8d3216e24e1d3dddaae86025
user <nadya11090@gmail.com>
2021-10-27 20:39:34 +0300

    I corrected the comments

--- src/main/java/tasks/homework/days7_8/package1/objects/Sand.java
+++ src/main/java/tasks/homework/days7_8/package1/objects/Sand.java
@@ -1,7 +1,50 @@
 package tasks.homework.days7_8.package1.objects;
 
+import java.util.Objects;
+
 public class Sand {
     private int weight;
     private String name;
 
+    public Sand(int weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Sand)) return false;
+        Sand sand = (Sand) o;
+        return getWeight() == sand.getWeight() && Objects.equals(getName(), sand.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getWeight(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Sand{" +
+                "weight=" + weight +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
