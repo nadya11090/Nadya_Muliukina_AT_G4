f2f059aaa63695ce1ab68ac98f6c594fc2d89af7
user <nadya11090@gmail.com>
2021-11-19 15:00:38 +0300

    added thread task

--- src/main/java/tasks/homework/Threadtask/Hedgehog.java
+++ src/main/java/tasks/homework/Threadtask/Hedgehog.java
@@ -1,4 +1,21 @@
 package tasks.homework.Threadtask;
 
 public class Hedgehog {
+
+    private String pattern = "Hedgehog <%d>";
+    private String name;
+
+    public Hedgehog(int number) {
+        this.name = String.format(pattern, number);
+    }
+
+    public void tellMe() {
+
+        System.out.printf(String.format("I am %s! ", this.name));
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/tasks/homework/Threadtask/Mouse.java
+++ src/main/java/tasks/homework/Threadtask/Mouse.java
@@ -2,4 +2,21 @@ package tasks.homework.Threadtask;
 
 public class Mouse {
 
+    private String pattern = "Mouse <%d>";
+    private String name;
+
+    public Mouse(int number) {
+        this.name = String.format(pattern, number);
+    }
+
+    public void peep() {
+
+        System.out.printf(String.format("\n" + "%s PEEP ", this.name));
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
